ae75a5c3f2363bad2714f6a5d9ebe4a1ad61a1b1
Leah <UTP0212675@ALUMNO.UTPUEBLA.EDU.MX>
2025-09-02 22:29:32 -0600

    TDA se agregaron imagenes y actualizacion de interfaz

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnAtender = new System.Windows.Forms.Button();
+            this.lblAtendido = new System.Windows.Forms.Label();
+            this.lstPacientes = new System.Windows.Forms.ListBox();
+            this.txtPaciente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(438, 138);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(106, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar Paciente";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click_1);
+            // 
+            // btnAtender
+            // 
+            this.btnAtender.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnAtender.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtender.Location = new System.Drawing.Point(327, 258);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(102, 23);
+            this.btnAtender.TabIndex = 1;
+            this.btnAtender.Text = "Atender Paciente";
+            this.btnAtender.UseVisualStyleBackColor = false;
+            this.btnAtender.Click += new System.EventHandler(this.btnAtender_Click_1);
+            // 
+            // lblAtendido
+            // 
+            this.lblAtendido.AutoSize = true;
+            this.lblAtendido.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAtendido.Location = new System.Drawing.Point(198, 297);
+            this.lblAtendido.Name = "lblAtendido";
+            this.lblAtendido.Size = new System.Drawing.Size(0, 13);
+            this.lblAtendido.TabIndex = 2;
+            // 
+            // lstPacientes
+            // 
+            this.lstPacientes.BackColor = System.Drawing.Color.PaleGreen;
+            this.lstPacientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPacientes.FormattingEnabled = true;
+            this.lstPacientes.Location = new System.Drawing.Point(201, 186);
+            this.lstPacientes.Name = "lstPacientes";
+            this.lstPacientes.Size = new System.Drawing.Size(120, 95);
+            this.lstPacientes.TabIndex = 3;
+            // 
+            // txtPaciente
+            // 
+            this.txtPaciente.BackColor = System.Drawing.Color.PaleGreen;
+            this.txtPaciente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPaciente.Location = new System.Drawing.Point(201, 138);
+            this.txtPaciente.Name = "txtPaciente";
+            this.txtPaciente.Size = new System.Drawing.Size(211, 20);
+            this.txtPaciente.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(174, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(417, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hospital Veterinario El Michi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ingrese el nombre del paciente: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(88, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Pacientes en espera:";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.RosyBrown;
+            this.pictureBox4.Image = global::TDA_queue.Properties.Resources.perro;
+            this.pictureBox4.Location = new System.Drawing.Point(576, 308);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(226, 205);
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.RosyBrown;
+            this.pictureBox3.Image = global::TDA_queue.Properties.Resources.tortuga;
+            this.pictureBox3.Location = new System.Drawing.Point(597, -18);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(205, 174);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TDA_queue.Properties.Resources.pez;
+            this.pictureBox2.Location = new System.Drawing.Point(-52, -60);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(189, 186);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TDA_queue.Properties.Resources.gato;
+            this.pictureBox1.Location = new System.Drawing.Point(-16, 286);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 227);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPaciente);
+            this.Controls.Add(this.lstPacientes);
+            this.Controls.Add(this.lblAtendido);
+            this.Controls.Add(this.btnAtender);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnAtender;
+        private System.Windows.Forms.Label lblAtendido;
+        private System.Windows.Forms.ListBox lstPacientes;
+        private System.Windows.Forms.TextBox txtPaciente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,86 @@ namespace TDA_queue
 {
     public partial class Form1 : Form
     {
+        private string[] cola;
+        private int front;
+        private int rear;
+        private int count;
         public Form1()
         {
             InitializeComponent();
+
+            int capacidad = 50; // Tamaño maximo de la cola
+            cola = new string[capacidad];
+            front = 0;
+            rear = -1;
+            count = 0;
+        }
+        private void btnAgregar_Click_1(object sender, EventArgs e)
+        {
+            //este boton agrega al paciente a la cola
+            ////Encolar
+            string paciente = txtPaciente.Text.Trim();
+
+            if (paciente == "")
+            {
+                MessageBox.Show("Escribe el nombre del paciente.");
+                return;
+            }
+
+            if (count == cola.Length)
+            {
+                MessageBox.Show("La cola está llena.");
+                return;
+            }
+
+            rear = (rear + 1) % cola.Length; // Avanzar circularmente
+            cola[rear] = paciente;
+            count++;
+
+            txtPaciente.Clear();
+            MostrarCola();
+        }
+        private void btnAtender_Click_1(object sender, EventArgs e)
+        {
+            //este boton atiende al paciente que esta en la cola
+            //Desencolar
+            if (count == 0)
+            {
+                MessageBox.Show("No hay pacientes en espera.");
+                return;
+            }
+
+            string atendido = cola[front];
+            front = (front + 1) % cola.Length; // Avanza circularmente
+            count--;
+            //Muestra el paciente atendido
+            lblAtendido.Text = "Atendiendo a: " + atendido;
+            MostrarCola();
+
+        }
+        // Mostrar la cola en el ListBox
+        private void MostrarCola()
+        {
+            lstPacientes.Items.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                int index = (front + i) % cola.Length;
+                lstPacientes.Items.Add(cola[index]);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
+    
 }
